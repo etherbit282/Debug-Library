8ee5a87be2f96f4613c2d952bba964a04655db4a
etherbit <edward.ashley217@outlook.com>
2025-07-02 20:21:44 +0100

    Added create console

--- Debug Library/Program.cs
+++ Debug Library/Program.cs
@@ -1,31 +1,63 @@
-﻿namespace DebugLibrary
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DebugLibrary
 {
     public static class DebugLib
     {
-        public enum LogLevel { Info, Warning, Error }
+        public enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
 
         public static LogLevel CurrentLevel { get; set; } = LogLevel.Info;
 
+        private static bool _consoleAllocated = false;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        public static void EnableConsole()
+        {
+            if (_consoleAllocated)
+                return;
+
+            AllocConsole();
+            _consoleAllocated = true;
+        }
+
         public static void Log(string message, LogLevel level = LogLevel.Info)
         {
-            if (level < CurrentLevel) return;
+            if (level < CurrentLevel)
+                return;
 
-            ConsoleColor originalColor = Console.ForegroundColor;
+            if (!_consoleAllocated)
+            {
+                // console not allocated yet so log nothing
+                return;
+            }
+
+            var originalColor = Console.ForegroundColor;
 
             switch (level)
             {
                 case LogLevel.Info:
                     Console.ForegroundColor = ConsoleColor.Blue;
                     break;
+
                 case LogLevel.Warning:
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     break;
+
                 case LogLevel.Error:
                     Console.ForegroundColor = ConsoleColor.Red;
                     break;
             }
 
             Console.WriteLine($"[{level}] {message}");
+
             Console.ForegroundColor = originalColor;
         }
     }

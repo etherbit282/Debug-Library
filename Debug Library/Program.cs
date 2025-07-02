ec1e0d1ae6ea58680b9e72974c8154f04431f692
etherbit <edward.ashley217@outlook.com>
2025-07-02 20:08:26 +0100

    Add project files.

---
+++ Debug Library/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DebugLibrary
+{
+    public static class DebugLib
+    {
+        public enum LogLevel { Info, Warning, Error }
+
+        public static LogLevel CurrentLevel { get; set; } = LogLevel.Info;
+
+        public static void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            if (level < CurrentLevel) return;
+
+            ConsoleColor originalColor = Console.ForegroundColor;
+
+            switch (level)
+            {
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.WriteLine($"[{level}] {message}");
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
